7f00a673bfeff08ee9064e4036d7e5d4e6718d63
Marzia Faustinelli <marzia.faustinelli@gmail.com>
2020-05-31 00:36:59 +0200

    animations, and basic attacker/spawner

---
+++ Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class AttackerSpawner : MonoBehaviour
+{
+    [SerializeField] float spawnIntervalMin = 1f;
+    [SerializeField] float spawnIntervalMax = 5f;
+    [SerializeField] Attacker attackerPrefab;
+
+    bool spawning = true;
+
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        while (spawning)
+        {
+            yield return new WaitForSeconds(RandSpawnTime());
+            SpawnAttacker();
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private float RandSpawnTime()
+    {
+        return Random.Range(spawnIntervalMin, spawnIntervalMax);
+    }
+
+    private void SpawnAttacker()
+    {
+        Instantiate(attackerPrefab, transform.position, transform.rotation);
+
+    }
+}
