0a5e2e765bc8935d98c368d9828909f6bfe6838e
Marzia Faustinelli <marzia.faustinelli@gmail.com>
2020-06-13 00:24:20 +0200

    spawn defenders on mouse click

---
+++ Assets/Scripts/DefenderSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderSpawner: MonoBehaviour
+{
+    [SerializeField] Defender defender;
+
+    private void OnMouseDown()
+    {
+        SpawnDefender(GetSquareCentre());
+    }
+
+    private void SpawnDefender(Vector2 spawnPos)
+    {
+        Instantiate(defender, spawnPos, Quaternion.identity);
+    }
+
+    private Vector2 GetSquareCentre()
+    {
+        var clickPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        float squareX = Mathf.RoundToInt(clickPos.x);
+        float squareY = Mathf.RoundToInt(clickPos.y);
+
+        var squareCentre = new Vector2(squareX, squareY);
+        return squareCentre;
+    }
+}
