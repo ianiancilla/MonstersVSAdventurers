e94ccff86bcf75feab4ccba93e3e8ddffa22c1ed
Marzia Faustinelli <marzia.faustinelli@gmail.com>
2020-06-18 22:46:10 +0200

    added resource system start

---
+++ Assets/Scripts/ResourceDisplay.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourceDisplay : MonoBehaviour
+{
+    [SerializeField] int resourceCount = 100;
+    TextMeshProUGUI resourceDisplay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        resourceDisplay = GetComponent<TextMeshProUGUI>();
+        UpdateDisplay();
+    }
+
+    private void UpdateDisplay()
+    {
+        resourceDisplay.text = resourceCount.ToString();
+    }
+
+    public void AddResources(int amount)
+    {
+        resourceCount += amount;
+        UpdateDisplay();
+    }
+
+    public void SpendResources(int amount)
+    {
+        if (amount <= resourceCount)
+        {
+            resourceCount -= amount;
+        }
+        UpdateDisplay();
+    }
+}
