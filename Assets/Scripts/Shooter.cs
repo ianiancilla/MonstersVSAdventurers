2ab2a4a2ba3c14fea483cdd883d34cbf36f38f53
Marzia Faustinelli <marzia.faustinelli@gmail.com>
2020-07-07 20:02:44 +0200

    logic behind animation change for shooters

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -6,8 +6,57 @@ public class Shooter : MonoBehaviour
 {
     [SerializeField] GameObject projectile;
     [SerializeField] Transform projectile_origin;
+
+    AttackerSpawner myLaneSpawner;
+
+    public void Start()
+    {
+        SetLaneSpawner();
+    }
+
+    public void Update()
+    {
+        if (isEnemyInLane())
+        {
+            //TODO set animation state to shooting
+        }
+        else
+        {
+            //TODO set animation state to idle
+        }
+    }
+
     public void Fire()
     {
         Instantiate(projectile, projectile_origin.position, Quaternion.identity);
     }
+
+    private void SetLaneSpawner()
+    {
+        AttackerSpawner[] attackerSpawners = FindObjectsOfType<AttackerSpawner>();
+
+        foreach (var spawner in attackerSpawners)
+        {
+            bool isCloseEnough = Mathf.Abs(spawner.transform.position.y - transform.position.y) 
+                                    <= Mathf.Epsilon;
+            if (isCloseEnough)
+            {
+                myLaneSpawner = spawner;
+                return;
+            }
+        }
+    }
+
+    private bool isEnemyInLane()
+    {
+        if (myLaneSpawner.transform.childCount > 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
 }
